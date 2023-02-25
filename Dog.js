d07c6e61950ef4dc79c108d07263e3d1cbcd070b
Ying (Alice) <ying.pan1990@gmail.com>
2023-02-24 21:37:37 -0500

    new a file

---
+++ Dog.js
@@ -0,0 +1,28 @@
+class Dog {
+    constructor(data) {
+        Object.assign(this, data)
+    }
+    
+    getBadgeHtml(swipe, like) {
+        return (!like && swipe) ? `<img src="images/badge-nope.png" class="badge" id="badge-nope">` 
+        :  like ? `<img src="images/badge-like.png" class="badge" id="badge-like">` 
+        :  ""
+    }
+    
+    getDogHtml() {
+        const {name, avatar, age, bio} = this 
+        const badgeHtml = this.getBadgeHtml(this.hasBeenSwiped, this.hasBeenLiked)
+        return `
+            <div class="main">
+                <img src="${avatar}" class="dog-img">
+                <div class="dog-detail">
+                    <h3>${name}, ${age}</h3>
+                    <p>${bio}</p>
+                </div>  
+                ${badgeHtml}
+            </div>
+        `
+    }
+}
+
+export default Dog
---
+++ index.js
@@ -0,0 +1,68 @@
+import dogs from "./data.js"
+import Dog from "./Dog.js"
+
+function getEndMessage() {
+    isWaiting = true
+    setTimeout(() => {
+        document.body.innerHTML = `
+            <h3 class="last-page">There's no more dog in the photo album.</h3>
+            <img class="end-img" src="https://media2.giphy.com/media/l4FAPaGGeB7D1LfIA/giphy.gif?cid=ecf05e47w06cgl81qe0ky59oxijv9i56rs0gzbb0au5gxbpg&rid=giphy.gif&ct=g">
+            <button class="btn" id="btn">Return</button>
+            ` 
+        document.getElementById("btn").addEventListener("click", () => {
+            location.reload() 
+        })
+    },1500)   
+}
+
+function getNewDog() {
+    const nextDog = dogs.shift()
+    return nextDog ? new Dog(nextDog) : {}
+}
+
+let isWaiting = false
+document.getElementById("cross").addEventListener("click", () => {
+    if(!isWaiting) {
+        isWaiting = true
+        dog.hasBeenSwiped = true
+        dog.hasBeenLiked = false
+        render()
+        if(dogs.length > 0) {
+            setTimeout(() => {
+                isWaiting = false   
+                dog = getNewDog()
+                render()   
+            },1000)
+        }
+        else {
+            getEndMessage()
+        }
+    } 
+})
+
+document.getElementById("heart").addEventListener("click", () => {
+    if(!isWaiting) {
+        isWaiting = true
+        dog.hasBeenSwiped = true
+        dog.hasBeenLiked = true
+        render()
+        if(dogs.length > 0) {
+            setTimeout(() => {
+                isWaiting = false   
+                dog = getNewDog()
+                render()   
+            },1000)
+        }
+        else {
+            getEndMessage()
+        }
+    }
+})
+
+function render(){
+    document.getElementById("main").innerHTML = dog.getDogHtml()
+}
+
+let dog = getNewDog()
+render()   
+   
