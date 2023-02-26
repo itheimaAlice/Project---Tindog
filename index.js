e39d0cbd909e5149f3f30102b2451c23ceac5803
Alice <116904147+itheimaAlice@users.noreply.github.com>
2023-02-26 11:38:51 -0500

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function getEndMessage() {
     setTimeout(() => {
         document.body.innerHTML = `
             <h3 class="last-page">You have swiped all the dogs.</h3>
-            <img class="end-img" src="https://media2.giphy.com/media/l4FAPaGGeB7D1LfIA/giphy.gif?cid=ecf05e47w06cgl81qe0ky59oxijv9i56rs0gzbb0au5gxbpg&rid=giphy.gif&ct=g">
+            <img class="end-img" src="https://media2.giphy.com/media/l4FAPaGGeB7D1LfIA/giphy.gif?cid=ecf05e47w06cgl81qe0ky59oxijv9i56rs0gzbb0au5gxbpg&rid=giphy.gif&ct=g" />
             <button class="btn" id="btn">Return</button>
             ` 
         document.getElementById("btn").addEventListener("click", () => {
